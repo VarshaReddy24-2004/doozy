5e9348c4f28901df702ef4524c1f4796193ff20b
salman <varshareddyr75@gmail.com>
2025-05-15 12:50:38 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/doozy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doozy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:doozy/pages/login_page.dart';
+import 'package:doozy/providers/login_provider.dart'; // Make sure this path is correct
+import 'package:doozy/providers/task_provider.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const LoginPage(),
+      ),
+    );
+  }
+}
